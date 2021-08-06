4aa0447a8f33d35c2b8121ead21fc523b3cd5c0a
BOupdown <75842770+BOupdown@users.noreply.github.com>
2021-08-06 17:59:45 +0200

    8/6/2021

---
+++ letterCombinationsOfAPhoneNumber.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+class Solution{
+public:
+    vector<string> letterCombinations(string digits)
+    {
+        if(digits.size() == 0)
+            return {};
+
+        string tab[] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        vector<string> res;
+
+        letterCombinationsRecursive(res, digits, "", 0, tab);
+
+        return res;
+    }
+
+    void letterCombinationsRecursive(vector<string>& result, string digits, string current, int index, string* tab)
+    {
+        if(index == digits.size())
+        {
+            result.push_back(current);
+            return;
+        }
+
+        string letters = tab[digits[index] - '0'];
+
+        for(int i = 0; i < letters.size(); i++)
+        {
+            letterCombinationsRecursive(result, digits, current + letters[i], index + 1, tab);
+        }
+    }
+};
+
+
+
