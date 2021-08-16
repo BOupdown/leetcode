9054bbf8601de86b26ceb8930863834653393c30
BOupdown <75842770+BOupdown@users.noreply.github.com>
2021-08-16 14:59:19 +0200

    8/16/2021

---
+++ validParentheses.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+class Solution{
+public:
+    bool isValid(string s)
+    {
+        if(s.size() == 0 || s.size() == 1)
+            return false;
+
+        vector<char> ret;
+        unordered_map<char, char> hash;
+        
+        hash['('] = ')';
+        hash[')'] = 'a';
+        hash[']'] = 'b';
+        hash['['] = ']';
+        hash['}'] = 'c';
+        hash['{'] = '}';
+
+    
+        for(int i = 0; i < s.size(); i++)
+        {
+            if(ret.size() == 0 || hash[ret[ret.size() - 1]] != s[i])
+                ret.push_back(s[i]);
+            else
+                ret.pop_back();
+        }
+
+        if(ret.size() == 0)
+            return true;
+        else
+            return false;
+    }
+};
