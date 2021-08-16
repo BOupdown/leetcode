361fa4b66ec33657332a859ca972e6bed25dc402
BOupdown <75842770+BOupdown@users.noreply.github.com>
2021-08-16 15:18:39 +0200

    8/16/2021

---
+++ mergeTwoSortedLists.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        
+        if(l1 == NULL && l2 == NULL)
+            return NULL;
+        
+        ListNode* temp1 = l1;
+        ListNode* temp2 = l2;
+        ListNode* res = new ListNode;
+        ListNode* tempRes = res;
+        
+        while(temp1 != NULL || temp2 != NULL)
+        {
+            if(temp1 != NULL && temp2 != NULL)
+            {
+                if(temp1->val < temp2->val)
+                {
+                    tempRes->val = temp1->val;
+                    temp1 = temp1->next;
+                }else{
+                    tempRes->val = temp2->val;
+                    temp2 = temp2->next;
+                }
+            }
+            else if(temp1 == NULL)
+            {
+                tempRes->val = temp2->val;
+                temp2 = temp2->next;
+            }else{
+                tempRes->val = temp1->val;
+                temp1 = temp1->next;
+            }
+            
+            if(temp1 != NULL || temp2 != NULL)
+            {
+                tempRes->next = new ListNode;
+                tempRes = tempRes->next;
+            }
+            
+        }
+        
+        return res;
+    }
+};
