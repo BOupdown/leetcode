60799b680c679be6dddf91cca31835ac3748c1d8
BOupdown <75842770+BOupdown@users.noreply.github.com>
2021-08-23 21:52:12 +0200

    Update addTwoNumbers.cpp

--- addTwoNumbers.cpp
+++ addTwoNumbers.cpp
@@ -1,12 +1,4 @@
-/*You are given two non-empty linked lists representing two non-negative integers.
- *
- * The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
- *
- * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
- *
- */
-
-
+//      https://leetcode.com/problems/add-two-numbers/
 #include <iostream>
 
 using namespace std;
@@ -69,12 +61,3 @@ public:
         return retour;
     }
 };
-
-
-int main()
-{
-    Solution somme;
-
-    return 0;
-
-}
